917a5a24c7697a73fe22eeeb679109fe78fbf21b
Talu Kara <h7yt0p@duck.com>
2024-03-28 17:43:38 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cstdlib>
+#include <string>
+
+int main() {
+    // Automatically retrieve user's IP address
+    std::string command = "curl ifconfig.me";
+    FILE* fp = popen(command.c_str(), "r");
+    if (!fp) {
+        std::cerr << "Error occurred while executing command!" << std::endl;
+        return 1;
+    }
+
+    char buffer[1024];
+    std::string ip_address = "";
+    while (fgets(buffer, sizeof(buffer), fp)) {
+        ip_address += buffer;
+    }
+    pclose(fp);
+
+    // Provide information about the IP address
+    command = "curl ipinfo.io/" + ip_address;
+    std::cout << "IP Address Information:" << std::endl;
+    system(command.c_str());
+
+    return 0;
+}
